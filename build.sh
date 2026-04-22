7bfb57a14528423943a34c8a54196d2de80334d4
Stephen Von Worley <svonworl@ucsc.edu>
2026-04-22 09:41:45 -0700

    rename script, tweak build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$ROOT_DIR"
+cd "${ROOT_DIR}"
 
-WORK_DIR=${ROOT_DIR}/build
+WORK_DIR=${ROOT_DIR}/work
 mkdir -p ${WORK_DIR}
 
 EDAM_TAG="1.25-20251112T1620Z-intermediate"
@@ -13,13 +13,13 @@ OWL_FILE="${WORK_DIR}/EDAM.owl"
 SIMPLIFIED_FILE="${WORK_DIR}/EDAM_simplified.json"
 
 # Get a recent tagged release of the EDAM ontology
-echo "Downloading $OWL_FILE from tag $EDAM_TAG..."
-curl -fsSL -o "$OWL_FILE" "$OWL_URL"
+echo "Downloading $OWL_FILE from tag ${EDAM_TAG}..."
+curl -fsSL -o "${OWL_FILE}" "${OWL_URL}"
 
 # Convert the EDAM XML file to a simplified JSON representation.
-python convert.py < ${OWL_FILE} > ${SIMPLIFIED_FILE}
+python simplify.py < ${OWL_FILE} > ${SIMPLIFIED_FILE}
 
 # Create a file for each subontology of interest using the simplified EDAM JSON representation.
 python extract.py < ${SIMPLIFIED_FILE}
 
-echo "Done. JSON files written to $ROOT_DIR."
+echo "Done. JSON files written to ${ROOT_DIR}."
