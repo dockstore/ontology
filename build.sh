6cda629b0558a79d9e4e509a1982ad47fd0b82a3
Steve Von Worley <svonworl@ucsc.edu>
2026-04-23 13:33:18 -0700

    create ontologies for use in AI auto-categorization
    https://ucsc-cgl.atlassian.net/browse/SEAB-7540

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ROOT_DIR="$(cd "$(dirname "$0")" && pwd)"
+WORK_DIR=${ROOT_DIR}/work
+GENERATED_DIR=${ROOT_DIR}/generated
+
+cd "${ROOT_DIR}"
+mkdir -p ${WORK_DIR}
+mkdir -p ${GENERATED_DIR}
+
+EDAM_TAG="1.25-20251112T1620Z-intermediate"
+OWL_URL="https://github.com/edamontology/edamontology/releases/download/${EDAM_TAG}/EDAM.owl"
+OWL_FILE="${WORK_DIR}/EDAM.owl"
+SIMPLIFIED_FILE="${WORK_DIR}/EDAM_simplified.json"
+SIMPLIFIED_AMERICANIZED_FILE="${WORK_DIR}/EDAM_simplified_americanized.json"
+
+# Get a recent tagged release of the EDAM ontology
+echo "Downloading $OWL_FILE from tag ${EDAM_TAG}..."
+curl -fsSL -o "${OWL_FILE}" "${OWL_URL}"
+
+# Convert the EDAM XML file to a simplified JSON representation.
+echo "Simplifying..."
+python3 simplify.py < ${OWL_FILE} > ${SIMPLIFIED_FILE}
+
+# Convert British spellings to American spellings.
+echo "Americanizing..."
+python3 americanize.py < ${SIMPLIFIED_FILE} > ${SIMPLIFIED_AMERICANIZED_FILE}
+
+# Create a file for each subontology of interest using the simplified+Americanized EDAM JSON representation.
+echo "Extracting..."
+python3 extract.py ${GENERATED_DIR} < ${SIMPLIFIED_AMERICANIZED_FILE}
+
+echo "Done. Ontology JSON files written to ${GENERATED_DIR}."
---
+++ extract.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Extract a file for each of the subontologies that we will use to automatically categorize Dockstore entries, using the simplified EDAM JSON representation as a base, and modifying as necessary."""
+
+import json
+import sys
+
+def extract_subtree(nodes, root_id):
+    id_to_node = {node['id']: node for node in nodes}
+    def in_tree(node):
+        if node['id'] == root_id:
+            return True
+        for parent in node['parent_ids']:
+            parent_node = id_to_node.get(parent)
+            if (parent_node and in_tree(parent_node)):
+                return True
+        return False
+    return [node for node in nodes if in_tree(node)]
+
+def remove_subtree(nodes, root_id):
+    remove_ids = {node['id'] for node in extract_subtree(nodes, root_id)}
+    return [{**node, 'parent_ids': [p for p in node['parent_ids'] if p not in remove_ids]} for node in nodes if node['id'] not in remove_ids]
+
+def add_prefix_to_ids(nodes, prefix):
+    return [{**node, 'id': prefix + node['id'], 'parent_ids': [prefix + p for p in node['parent_ids']]} for node in nodes]
+
+def make_only_leaves_categorical(nodes):
+    parent_ids = set()
+    for node in nodes:
+        parent_ids.update(node['parent_ids'])
+    return [{**node, 'categorical': node['categorical'] and node['id'] not in parent_ids} for node in nodes]
+
+def get_generated_dir():
+    if len(sys.argv) <= 1:
+       print(f'Usage: {sys.argv[0]} <generated_dir>', file=sys.stderr)
+       sys.exit(1)
+    return sys.argv[1]
+
+def write_json(nodes, filename):
+    with open(filename, 'w') as f:
+        json.dump(nodes, f, indent=4)
+    print(f"Wrote {len(nodes)} nodes to {filename}", file=sys.stderr)
+
+
+def main():
+    generated_dir = get_generated_dir()
+    nodes = json.load(sys.stdin)
+
+    # Use the "operation" and "topic" subontologies verbatim.
+    write_json(extract_subtree(nodes, 'operation'), f'{generated_dir}/operation.json')
+    write_json(extract_subtree(nodes, 'topic'), f'{generated_dir}/topic.json')
+
+    # Remove the "identifier" sub-branch of the "data" subontology,
+    # then write versions of it for both inputs and outputs.
+    data = remove_subtree(extract_subtree(nodes, 'data'), 'data-identifier')
+    write_json(add_prefix_to_ids(data, 'input-'), f'{generated_dir}/input-data.json')
+    write_json(add_prefix_to_ids(data, 'output-'), f'{generated_dir}/output-data.json')
+
+    # Change the "format" subontology so that only the leaves are categorical,
+    # then write versions of it for both inputs and outputs.
+    format = make_only_leaves_categorical(extract_subtree(nodes, 'format'))
+    write_json(add_prefix_to_ids(format, 'input-'), f'{generated_dir}/input-format.json')
+    write_json(add_prefix_to_ids(format, 'output-'), f'{generated_dir}/output-format.json')
+
+
+if __name__ == '__main__':
+    main()
