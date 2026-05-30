bb0882b9eb815bad6216d7e1478b543d46bbb9a8
Stephen Von Worley <svonworl@ucsc.edu>
2026-05-29 19:58:57 -0700

    improve naming in build.sh

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ OWL_URL="https://github.com/edamontology/edamontology/releases/download/${EDAM_T
 OWL_FILE="${WORK_DIR}/EDAM.owl"
 SIMPLIFIED_FILE="${WORK_DIR}/EDAM_simplified.json"
 SIMPLIFIED_CORRECTED_FILE="${WORK_DIR}/EDAM_simplified_corrected.json"
-SIMPLIFIED_AMERICANIZED_FILE="${WORK_DIR}/EDAM_simplified_americanized.json"
+SIMPLIFIED_CORRECTED_AMERICANIZED_FILE="${WORK_DIR}/EDAM_simplified_corrected_americanized.json"
 
 # Get a recent tagged release of the EDAM ontology
 echo "Downloading $OWL_FILE from tag ${EDAM_TAG}..."
@@ -30,10 +30,10 @@ python3 correct.py < ${SIMPLIFIED_FILE} > ${SIMPLIFIED_CORRECTED_FILE}
 
 # Convert British spellings to American spellings.
 echo "Americanizing..."
-python3 americanize.py < ${SIMPLIFIED_CORRECTED_FILE} > ${SIMPLIFIED_AMERICANIZED_FILE}
+python3 americanize.py < ${SIMPLIFIED_CORRECTED_FILE} > ${SIMPLIFIED_CORRECTED_AMERICANIZED_FILE}
 
 # Create a file for each subontology of interest using the simplified+corrected+Americanized EDAM JSON representation.
 echo "Extracting..."
-python3 extract.py ${GENERATED_DIR} < ${SIMPLIFIED_AMERICANIZED_FILE}
+python3 extract.py ${GENERATED_DIR} < ${SIMPLIFIED_CORRECTED_AMERICANIZED_FILE}
 
 echo "Done. Ontology JSON files written to ${GENERATED_DIR}."
